eb7c96ce68768079bb5d8e4ab91a5279bd519f20
semcold71 <semcold@gmail.com>
2022-01-08 11:33:13 +0300

    Add FXML functionality, add Padeg.dll

--- src/main/java/ru/samcold/classify/SpringApplication.java
+++ src/main/java/ru/samcold/classify/SpringApplication.java
@@ -1,12 +1,12 @@
 package ru.samcold.classify;
 
+import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringApplication {
 
 	public static void main(String[] args) {
-		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+		Application.launch(FormApplication.class, args);
 	}
-
 }
---
+++ src/main/java/ru/samcold/classify/controllers/MainController.java
@@ -0,0 +1,15 @@
+package ru.samcold.classify.controllers;
+
+import javafx.fxml.FXML;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component
+@FxmlView(value = "/fxml/main.fxml")
+public class MainController {
+
+    @FXML
+    public void initialize() {
+
+    }
+}
