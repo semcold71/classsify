d8f31646cd2981408c699b4152104a123bc3c8da
semcold71 <semcold@gmail.com>
2022-01-08 12:50:03 +0300

    Add and design groupView.fxml.fxml, add GroupController

---
+++ src/main/java/ru/samcold/classify/controllers/GroupController.java
@@ -0,0 +1,15 @@
+package ru.samcold.classify.controllers;
+
+import javafx.fxml.FXML;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component
+@FxmlView(value = "/fxml/groupView.fxml")
+public class GroupController {
+
+    @FXML
+    void initialize() {
+
+    }
+}
